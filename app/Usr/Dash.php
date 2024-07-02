7a6b04e166a60c2a53cd507a3ace75a06b43ba1f
luis <luisfernando.anemona@gmail.com>
2024-07-01 18:25:09 -0600

    ajuste portafolio

--- app/Usr/Dash.php
+++ app/Usr/Dash.php
@@ -5,6 +5,7 @@ class UsrDash
     public function index()
     {
         UtilHtml::set(
+            
             '/usr/html/site/index.phtml'
         );
         return get_defined_vars();
--- app/Usr/Template.php
+++ app/Usr/Template.php
@@ -28,34 +28,30 @@ class UsrTemplate
         AifHtml::head('link', array('rel' => "icon", 'type' => "image/png", 'sizes' => "192x192", 'href' => $favicon . '-300x300.png'));
         AifHtml::head('link', array('rel' => "apple-touch-icon", 'href' => $favicon . '-300x300.png'));
         AifHtml::setLineBreak("\r\n");
-        AifHtml::setHttpCss(
-            'https://fonts.gstatic.com',
-            'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap',
-            //'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css',
-        );
-        UtilHtml::css(
-            '/css/font-awesome-5.10.0.min.css',
-            '/lib/owlcarousel/assets/owl.carousel.min.css',
-            '/lib/lightbox/css/lightbox.min.css',
-            '/css/style.css'
-        );
+       
+       // UtilHtml::css(
+            //'/css/font-awesome-5.10.0.min.css',
+            //'/lib/owlcarousel/assets/owl.carousel.min.css',
+            //'/lib/lightbox/css/lightbox.min.css',
+            //'/css/style.css'
+       // );
 
-        UtilHtml::script(
+        //UtilHtml::script(
             //plugins
-            '/js/jquery-3.4.1.min.js',
-            '/js/bootstrap.bundle.min.js',
-            '/lib/typed/typed.min.js',
-            '/lib/easing/easing.min.js',
-            '/lib/waypoints/waypoints.min.js',
-            '/lib/owlcarousel/owl.carousel.min.js',
-            '/lib/isotope/isotope.pkgd.min.js',
-            '/lib/lightbox/js/lightbox.min.js',
-            '/mail/jqBootstrapValidation.min.js',
-            '/mail/contact.js',
+            //'/js/jquery-3.4.1.min.js',
+            //'/js/bootstrap.bundle.min.js',
+            //'/lib/typed/typed.min.js',
+            //'/lib/easing/easing.min.js',
+            //'/lib/waypoints/waypoints.min.js',
+            //'/lib/owlcarousel/owl.carousel.min.js',
+            //'/lib/isotope/isotope.pkgd.min.js',
+            //'/lib/lightbox/js/lightbox.min.js',
+            //'/mail/jqBootstrapValidation.min.js',
+            //'/mail/contact.js',
             //principal
-            '/js/main.js',
+            //'/js/main.js',
 
-        );
+        //);
         AifHtml::setSource('window.aif_assets="' . AifView::getPath() . '";');
         AifView::setLayout('/usr/html/layout.phtml');
         return get_defined_vars();
